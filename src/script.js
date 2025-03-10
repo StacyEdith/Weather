5a1094e07dfeefbe49b86299c6170a29bcf79a42
StacyEdith <stacyedithm@gmail.com>
2025-03-10 15:28:39 +0100

    Created JS file and added search engine functionality

---
+++ src/script.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchFormInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchFormInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
