b9d8562ec924961ffb7cc17ecd8b91ebe7aadbde
StacyEdith <stacyedithm@gmail.com>
2025-03-10 17:36:09 +0100

    Injected icon element

--- src/script.js
+++ src/script.js
@@ -7,7 +7,9 @@ function refreshWeather(response) {
   let windElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElememt = document.querySelector("#icon");
 
+  iconElememt.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
